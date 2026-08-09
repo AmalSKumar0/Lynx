86f7e087f59a80fdf3a154fe03e27c2bf6b1909e
AmalSKumar0 <amalskumarofficialz@gmail.com>
2026-08-09 21:41:14 +0530

    Evidence data passed to scoring agent then to identification engine returning clean data

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from lynx_engine.scoring_engine import scoreingEngine
 def main():
     # path = input("Enter the path:")
     evidence = scan("~/College Projects/noir/")
-    print("Evidence from collecting engine:",evidence.display())
     score = scoreingEngine(evidence)
-    print("Score from Scoring engine:",score.display())
     engine = IdentificationEngine(score)
     result = engine.identify()
     print("final data:")
